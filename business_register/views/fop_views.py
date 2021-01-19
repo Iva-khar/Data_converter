9a4e68131b01ea5ed5d97a2a9e6b36e3bebecb87
Iva-khar <ivakhar@gmail.com>
2021-01-19 17:22:38 +0200

    formatting .xlsx file

--- business_register/views/fop_views.py
+++ business_register/views/fop_views.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from openpyxl import Workbook
+from openpyxl.styles import Alignment, fonts, PatternFill
+from openpyxl.utils.cell import get_column_letter
 from rest_framework import viewsets
 
 from business_register.filters import FopFilterSet
@@ -50,14 +52,22 @@ def export_xlsx(queryset, export_file_path, export_dict, worksheet_title):
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.title = worksheet_title
+    worksheet.sheet_properties.tabColor = '0033CCCC'
+    worksheet.row_dimensions[1].height = 20
+    worksheet.freeze_panes = 'A2'
     for col_num, (column_title, query_field) in enumerate(export_dict.items(), 1):
         row_num = 1
         cell = worksheet.cell(row=row_num, column=col_num)
         cell.value = column_title
+        worksheet.column_dimensions[get_column_letter(col_num)].width = 30
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+        cell.font = fonts.Font(b=True)
+        cell.fill = PatternFill(bgColor='0033CCCC', fill_type="solid")
         for record in queryset:
             sell_value = getattr(record, query_field)
             row_num += 1
             cell = worksheet.cell(row=row_num, column=col_num)
+            cell.alignment = Alignment(vertical='top', wrap_text=True)
             try:
                 cell.value = sell_value
             except:
